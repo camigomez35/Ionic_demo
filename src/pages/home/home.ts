397089eea455a6538a5643a9891f4fee941598a8
Maria Camila  Gomez <mariacamilagomez@ventasd.apcmayorista.com>
2017-07-28 13:25:17 -0500

    Init

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,41 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  foto: any=""
+  existe: boolean = false
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController,
+              private camera: Camera) {
 
   }
+
+  saludar(){
+      let alert = this.alertCtrl.create({
+        title: "Hello",
+        subTitle: "Esto es Angular Medellin",
+        buttons: ["Ok"]
+      });
+      alert.present();
+  }
+
+  captura(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.foto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  compartir(){
+    console.log("Compartir")
+  }
+
 
 }
